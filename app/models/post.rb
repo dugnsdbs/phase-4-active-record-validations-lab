e052edebb33030bebe54d998d517e4c842bd7b86
yeohoon yoon <yhoon.yoon@gmail.com>
2022-03-15 09:36:51 -0400

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+   validates :title, presence: true
+   validates :content, length: { minimum: 250 }
+   validates :summary, length: { maximum: 250 }
+   validates :category, inclusion: { in: ['Fiction', "Non-Fiction"] }
+   
+   validate :must_have_words
+
+   def must_have_words
+    words = ["Won't Believe","Secret", "Top [number]", "Guess"].map{|word|word}
+
+    # unless title.match?(/Won't Believe/)
+   unless words.present?
+      errors.add(:title, "These words are not included, Wrong!!")
+    end
+  end
 end
